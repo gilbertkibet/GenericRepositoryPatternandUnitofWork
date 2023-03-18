1d824ae88a2abee8f046fa391960fe6e42dbccf5
Gilbert kibet <kibetgilly354@gmail.com>
2023-03-18 09:56:38 -0700

    finished implementing the generic repository class the in infrasture layer

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        T GetById(int id);
+        
+
+        List<T> GetAll();   
+
+        List<T> Find(Expression<Func<T, bool>> expression);
+        Task<List<T>> GetAllAsync(); 
+
+        void Add(T entity);
+
+        void AddRange(List<T> entities);
+
+        void Remove(T entity);  
+
+        void RemoveRange(List<T> entities);
+
+        void AddAsync(T entity);    
+
+        void Update(T entity);
+
+
+    }
+}
---
+++ Infrastructure/GenericRepository.cs
@@ -0,0 +1,71 @@
+﻿using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationContext _context;
+        public GenericRepository(ApplicationContext context)
+        {
+            _context = context; 
+        }
+        public void Add(T entity)
+        {
+           _context.Set<T>().Add(entity);
+        }
+
+        public void AddAsync(T entity)
+        {
+            _context.Set<T>().AddAsync(entity);
+        }
+
+        public void AddRange(List<T> entities)
+        {
+            _context.Set<T>().AddRange(entities);
+        }
+
+        public List<T> Find(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Where(expression).ToList();
+        }
+
+        public List<T> GetAll()
+        {
+          return _context.Set<T>().ToList();    
+        }
+
+        public Task<List<T>> GetAllAsync()
+        {
+            return _context.Set<T>().ToListAsync(); 
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void RemoveRange(List<T> entities)
+        {
+
+            _context.Set<T>().RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+    }
+}
